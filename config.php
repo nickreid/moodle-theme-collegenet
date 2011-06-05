0f173388e66c9dd8cd721250618df324da98d529
Nick Reid <nickwreid@gmail.com>
2011-06-05 04:09:19 -0700

    Minor Edits

--- config.php
+++ config.php
@@ -16,10 +16,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Configuration for Moodle's leatherbound theme.
+ * Configuration for Waiuku College's CollegeNET theme.
  *
- * DO NOT MODIFY THIS THEME!
- * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
+ * Based on 'Leatherbound' 
+ *
  *
  * For full information about creating Moodle themes, see:
  *  http://docs.moodle.org/en/Development:Themes_2.0
@@ -29,7 +29,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$THEME->name = 'leatherbound';
+$THEME->name = 'collegenet';
 
 ////////////////////////////////////////////////////
 // Name of the theme. Most likely the name of
@@ -239,5 +239,3 @@ $THEME->layouts = array(
 // Sets a custom render factory to use with the
 // theme, used when working with custom renderers.
 ////////////////////////////////////////////////////
-
-$THEME->custompix = true;
